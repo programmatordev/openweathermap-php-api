0f640fe5956a75ad50903d2b5ae99bd3ec461743
André Pimpão <a.pimpao@programmator.dev>
2023-06-22 18:57:36 +0100

    chore: refactored request into a single method

--- src/Endpoint/AbstractEndpoint.php
+++ src/Endpoint/AbstractEndpoint.php
@@ -3,25 +3,52 @@
 namespace ProgrammatorDev\OpenWeatherMap\Endpoint;
 
 use Http\Client\Common\HttpMethodsClient;
+use Http\Client\Exception;
+use ProgrammatorDev\OpenWeatherMap\HttpClient\ResponseMediator;
 use ProgrammatorDev\OpenWeatherMap\OpenWeatherMap;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
 
 class AbstractEndpoint
 {
-    public function __construct(protected OpenWeatherMap $api) {}
+    public function __construct(
+        protected OpenWeatherMap $api
+    ) {}
 
-    protected function getHttpClient(): HttpMethodsClient
+    /**
+     * @throws Exception
+     */
+    protected function sendRequest(
+        string $method,
+        string|UriInterface $baseUrl,
+        array $query = [],
+        array $headers = [],
+        string|StreamInterface $body = null
+    ): array
+    {
+        if ($baseUrl instanceof UriInterface) {
+            $baseUrl = (string) $baseUrl;
+        }
+
+        $uri = $this->createUrl($baseUrl, $query);
+
+        return ResponseMediator::toArray(
+            $this->getHttpClient()->send($method, $uri, $headers, $body)
+        );
+    }
+
+    private function getHttpClient(): HttpMethodsClient
     {
         return $this->api->getConfig()
             ->getHttpClientBuilder()
             ->getHttpClient();
     }
 
-    protected function createUrl(string $baseUrl, array $query): string
+    private function createUrl(string $baseUrl, array $query): string
     {
         // Add application key to all requests
         $query = $query + [
-            'appid' => $this->api->getConfig()
-                ->getApplicationKey()
+            'appid' => $this->api->getConfig()->getApplicationKey()
         ];
 
         return \sprintf('%s?%s', $baseUrl, http_build_query($query));
