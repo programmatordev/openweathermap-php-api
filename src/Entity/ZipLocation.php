d27a9fa008e94b29f0cbd064dde2a7ddffad368d
André Pimpão <a.pimpao@programmator.dev>
2023-06-22 16:37:49 +0100

    feat(test): added Geocoding tests

--- src/Entity/ZipLocation.php
+++ src/Entity/ZipLocation.php
@@ -4,7 +4,7 @@ namespace ProgrammatorDev\OpenWeatherMap\Entity;
 
 class ZipLocation
 {
-    private string $zip;
+    private string $zipCode;
 
     private string $name;
 
@@ -14,7 +14,7 @@ class ZipLocation
 
     public function __construct(array $data)
     {
-        $this->zip = $data['zip'];
+        $this->zipCode = $data['zip'];
         $this->name = $data['name'];
         $this->countryCode = $data['country'];
         $this->coordinate = new Coordinate([
@@ -23,9 +23,9 @@ class ZipLocation
         ]);
     }
 
-    public function getZip(): string
+    public function getZipCode(): string
     {
-        return $this->zip;
+        return $this->zipCode;
     }
 
     public function getName(): string
