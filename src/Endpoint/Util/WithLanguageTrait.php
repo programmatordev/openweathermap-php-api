62026386017e99f872ca19290add40942c2302e2
André Pimpão <a.pimpao@programmator.dev>
2023-08-11 17:34:30 +0100

    chore: updated endpoint traits validation

--- src/Endpoint/Util/WithLanguageTrait.php
+++ src/Endpoint/Util/WithLanguageTrait.php
@@ -4,14 +4,19 @@ namespace ProgrammatorDev\OpenWeatherMap\Endpoint\Util;
 
 use ProgrammatorDev\OpenWeatherMap\Language\Language;
 use ProgrammatorDev\OpenWeatherMap\Validator\ChoiceValidatorTrait;
+use ProgrammatorDev\YetAnotherPhpValidator\Exception\ValidationException;
+use ProgrammatorDev\YetAnotherPhpValidator\Validator;
 
 trait WithLanguageTrait
 {
     use ChoiceValidatorTrait;
 
+    /**
+     * @throws ValidationException
+     */
     public function withLanguage(string $language): static
     {
-        $this->validateChoice('language', $language, Language::getList());
+        Validator::choice(Language::getList())->assert($language, 'language');
 
         $clone = clone $this;
         $clone->language = $language;
